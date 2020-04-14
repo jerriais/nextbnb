981b861b91aab16a1578bcce5f403cf41a5340f0
Matt Thornington <mattbonn@gmail.com>
2020-04-13 23:09:17 -0400

    added [id].js

--- pages/houses/[id].js
+++ pages/houses/[id].js
@@ -1,7 +1,26 @@
-export default () => (
+import houses from "../houses.json";
+import Head from 'next/head'
+
+const House = props => (
     <div>
+        <Head>
+            <title>{props.house.title}</title>
+        </Head>
+        <img src={props.house.picture} width='100%' alt='House Picture' />
+        <p>{props.house.type} - {props.house.town}</p>
         <p>
-            Test
+            {props.house.title}
+        </p>
+        <p>
+            {props.house.rating} ({props.house.reviewsCount})
         </p>
     </div>
 )
+House.getInitialProps = ({ query }) => {
+    const {id} = query
+
+    return {
+        house: houses.filter(house => house.id === id)[0]
+    }
+}
+export default House
